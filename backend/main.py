764f50c3c21a850be3ec5776745562d31fd0f0c0
majnkraaft <169532258+majnkraaft@users.noreply.github.com>
2026-09-21 09:45:20 +0300

    update backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -82,7 +82,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://parcev.github.io/cukierek"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
