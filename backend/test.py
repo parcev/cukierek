0e22832704595771c53f5fbb9f7347e0c5974150
majnkraaft <169532258+majnkraaft@users.noreply.github.com>
2026-09-24 18:46:22 +0300

    update backend/test.py

--- backend/test.py
+++ backend/test.py
@@ -484,8 +484,7 @@ def start_session(
     req: StartSessionRequest
 ):
   
-    print("[START-SESSION] Frontend wysłał:", req.model_dump())
-
+    
     if not is_valid_platform(req.platform):
 
         raise HTTPException(
